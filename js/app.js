59ae5f26ada93047a67bcac4d657a4128718949f
nataliagarcia <nataliagarkcia@hotmail.com>
2016-09-22 00:51:54 -0400

    create js steps

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//create global variables and arrays necesaries for pictures and other contects
+//create 2 different on click functions that will call each array of images 
+//this will create  a div for the image and will add the boostrap css dinamicly
+//will create a function that will allow the user click on the picture and pop up a modal
+//grab the array with the pictures and make a for loop to be able to display those in the modal
+//create configuration for the contact form
+//grab values and send and ajax to be able to grab those
+//maybe tray to send thos out to an actual email
